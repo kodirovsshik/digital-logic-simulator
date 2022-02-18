8c073d14a0571a94c28f8f14db9f2792b3effe54
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2022-02-18 21:41:57 +0300

    Add project files.

---
+++ digilogsim/dependencies.cpp
@@ -0,0 +1,7 @@
+
+#pragma comment(lib, "libksn_window.lib")
+#pragma comment(lib, "libksn_window_gl.lib")
+#pragma comment(lib, "libksn_time.lib")
+
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glew32s.lib")
